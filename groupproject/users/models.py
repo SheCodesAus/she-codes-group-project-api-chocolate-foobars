d044e4c48674a6bef6cbbd071807d74995eae62b
lingtangg <60630600+lingtangg@users.noreply.github.com>
2022-09-01 10:58:49 +1000

    Added Users model

--- groupproject/users/models.py
+++ groupproject/users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+STATES = (
+  ('QLD', 'QLD'),
+  ('WA', 'WA'),
+  ('NSW', 'NSW'),
+)
+
+POSITIONS = (
+  ('Junior', 'Junior'),
+  ('Lead', 'Lead'),
+  ('Industry', 'Industry'),
+)
+
+STATUS = (
+  ('Application received', 'Application received'),
+  ('Unsucessful', 'Unsucessful'),
+  ('Ready for interview', 'Ready for interview'),
+  ('Position offered', 'Position offered'),
+  ('Active', 'Active'),
+  ('Inactive', 'Inactive'),
+)
+
+class CustomUser(AbstractUser):
+  phone_number = models.CharField(max_length=10, unique=True, blank=True, null=True)
+  cv = models.TextField(blank=True, null=True)
+  state = models.CharField(max_length=20, choices=STATES, blank=True, null=True)
+  years_industry_experience = models.IntegerField(blank=True, null=True)
+  interview_notes = models.TextField(default=None, blank=True, null=True)
+  feedback_for_mentors = models.TextField(default=None, blank=True, null=True)
+  mentor_comments = models.TextField(default=None, blank=True, null=True)
+  # update to foreign keys when position and status models are created
+  position = models.CharField(max_length=20, choices=POSITIONS, default=None, blank=True, null=True)
+  status = models.CharField(max_length=20, choices=STATUS, default='Application received', blank=True, null=True)
+
+  def __str__(self):
+    return self.username 
---
+++ groupproject/users/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth.hashers import make_password
+
+class CustomUserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    username = serializers.CharField(max_length=200)
+    email = serializers.CharField(max_length=200)
+    password = serializers.CharField(max_length=200)
+    phone_number = serializers.CharField(max_length=200)
+    state = serializers.CharField(max_length=200)
+    interview_notes = serializers.CharField(max_length=2000)
+    feedback_for_mentors = serializers.CharField(max_length=2000)
+    mentor_comments = serializers.CharField(max_length=2000)
+    position = serializers.CharField(max_length=200)
+    status = serializers.CharField(max_length=200)
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return CustomUser.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email',instance.email)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.state = validated_data.get('state', instance.state)
+        instance.interview_notes = validated_data.get('interview_notes', instance.interview_notes)
+        instance.feedback_for_mentors = validated_data.get('feedback_for_mentors', instance.feedback_for_mentors)
+        instance.mentor_comments = validated_data.get('mentor_comments', instance.mentor_comments)
+        instance.status = validated_data.get('status', instance.status)
+        
+        if "password" in validated_data.keys():
+            instance.password = make_password(validated_data.get('password'))
+        instance.save()
+        return instance
